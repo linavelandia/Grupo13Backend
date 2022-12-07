aec7d6c40dba7508834d97b0cb918a0af181b5cf
nelsonsegura <durca45@outlook.com>
2022-12-07 13:24:18 -0500

    first commit

--- src/main/java/com/app/MyMovies/interfaces/IClientRepository.java
+++ src/main/java/com/app/MyMovies/interfaces/IClientRepository.java
@@ -1,9 +1,15 @@
 package com.app.MyMovies.interfaces;
 
 import com.app.MyMovies.entities.Client;
+import com.app.MyMovies.entities.Movie;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 
 public interface IClientRepository extends MongoRepository<Client, String> {
+    @Query(value= "{name : ?0}") // SQL Equivalent : SELECT * FROM Movie select * from Movie where name=?
+    List<Client> getClientsByName(String name);
     
 }
